14ea2cc9f177d392e53dc9111eb609eb164af6da
Benoit <mx4yux@splm.siemens.com>
2019-10-17 17:29:51 +0200

    Adding a search view and some style

--- Components/Search.js
+++ Components/Search.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {View, TextInput, Button} from 'react-native';
+import { View, TextInput, Button } from 'react-native';
 
 class Search extends React.Component {
-  render(){
+  render() {
     return(
-      <View>
-        <TextInput placeholder='Rechercher'/>
+      <View style={{marginTop: 35}}>
+        <TextInput style={styles.textinput} placeholder='Rechercher'/>
         <Button title='Rechercher' onPress={() => {}}/>
       </View>
     )
+  }
+}
+
+const styles = {
+  textinput:{
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
   }
 }
 
